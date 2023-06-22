bb1f46a6c7ac0903a0c09829d521904816ea1b59
soumyyyaaa <soumya.agarwal1103@gmail.com>
2023-06-22 20:15:32 +0530

    Header and Navbar Added

---
+++ src/Components/Navbars.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import "./Navbars.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+function Navbars() {
+  return (
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div className="offcanvas-header">
+              <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Categories
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">Poetry</a></li>
+                    <li><a className="dropdown-item" href="#">Science Fiction</a></li>
+                    <li><a className="dropdown-item" href="#">Nonfiction</a></li>
+                    <li><a className="dropdown-item" href="#">Fantasy</a></li>
+                    <li><a className="dropdown-item" href="#">Mystery</a></li>
+                    <li><a className="dropdown-item" href="#">Biography</a></li>
+                  </ul>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Brands</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Best Sellers</a>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Pages
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><a className="dropdown-item" href="#">About Us</a></li>
+                    <li><a className="dropdown-item" href="#">Blog</a></li>
+                    <li><a className="dropdown-item" href="#">FAQ's</a></li>
+                    <li><a className="dropdown-item" href="#">Contact Us</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+  );
+}
+
+export default Navbars;
